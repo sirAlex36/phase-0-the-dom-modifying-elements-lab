71227abb35d6199d5cf6c4c67a4cb60994f25608
SirAlex36 <alexngenoo@gmail.com>
2025-09-24 14:59:15 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code here
+const mainElement = document.getElementById("main");
+mainElement.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Alex is the champion";
+document.body.appendChild(newHeader);
